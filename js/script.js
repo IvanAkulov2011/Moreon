60b36d8c9d43091c710e2bb98661ed6a7ece7193
IvanAkulov2011 <ivan.sharky.star@gmail.com>
2025-02-15 11:37:00 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,37 +1,37 @@
-window.addEventListener('DOMContentLoaded', ()=>{
+// window.addEventListener('DOMContentLoaded', ()=>{
 
-    const tabs_menu = document.querySelectorAll('.tab-menu li'),
-        tabs_secondMenuLi = document.querySelectorAll('.tab_secondMenu li'),
-        tabs_secondMenu = document.querySelectorAll('.tab_secondMenu'),
-          tabsContent = document.querySelectorAll('.tab-content'),
-          tabsPerent = document.querySelector('.tab-menu'),
-          tabsPerent2 = document.querySelectorAll('.tab_secondMenu');
+//     const tabs_menu = document.querySelectorAll('.tab-menu li'),
+//         tabs_secondMenuLi = document.querySelectorAll('.tab_secondMenu li'),
+//         tabs_secondMenu = document.querySelectorAll('.tab_secondMenu'),
+//           tabsContent = document.querySelectorAll('.tab-content'),
+//           tabsPerent = document.querySelector('.tab-menu'),
+//           tabsPerent2 = document.querySelectorAll('.tab_secondMenu');
 
           
-    function hidetabContent(){
-        tabsContent.forEach(item =>{
-            item.style.display = 'none';
-        });
-        tabs_secondMenu.forEach(item =>{
-            item.style.display = 'none';
-        });
-        tabs_menu.forEach(item =>{
-            item.classList.remove('tab-active');
-        });
-        tabs_secondMenuLi.forEach(item =>{
-            item.classList.remove('secondMenu-active');
-        });
-    }
+//     function hidetabContent(){
+//         tabsContent.forEach(item =>{
+//             item.style.display = 'none';
+//         });
+//         tabs_secondMenu.forEach(item =>{
+//             item.style.display = 'none';
+//         });
+//         tabs_menu.forEach(item =>{
+//             item.classList.remove('tab-active');
+//         });
+//         tabs_secondMenuLi.forEach(item =>{
+//             item.classList.remove('secondMenu-active');
+//         });
+//     }
 
-    function showTabContent(i = 0){
-        tabsContent[i].style.display = 'block';
-        tabs_menu[i].classList.add = ('tab-active');
-        tabs_secondMenu[i].style.display = 'flex';
-        tabs_secondMenuLi[i].classList.add = ('secondMenu-active');
-    }
+//     function showTabContent(i = 0){
+//         tabsContent[i].style.display = 'block';
+//         tabs_menu[i].classList.add = ('tab-active');
+//         tabs_secondMenu[i].style.display = 'flex';
+//         tabs_secondMenuLi[i].classList.add = ('secondMenu-active');
+//     }
 
-    hidetabContent();
-    showTabContent();
+//     hidetabContent();
+//     showTabContent();
 
 
-})
+// })
